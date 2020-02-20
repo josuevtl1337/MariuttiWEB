ea127649fee6dc503752fcf2fed584fd9bfb84b7
josuevtl1337 <josuevtl1337@gmail.com>
2020-02-20 14:21:31 -0300

    herramienta admin cambia entre las tablas onClick

--- src/components/header/admin/addRubro.js
+++ src/components/header/admin/addRubro.js
@@ -4,7 +4,6 @@ import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
