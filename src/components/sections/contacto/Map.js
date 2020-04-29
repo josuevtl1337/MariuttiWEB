e521554ed05e14f8a52c26cd890d5f0a6c1b1bff
josuevtl1337 <josuevtl1337@gmail.com>
2020-04-29 14:18:11 -0300

    commit to pull

--- src/components/sections/contacto/Map.js
+++ src/components/sections/contacto/Map.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { GoogleMap , withScriptjs, withGoogleMap, Marker } from "react-google-maps"
-import { InfoWindow } from 'google-maps-react';
 
 const Map = (props) =>{
     //-31.646097, -60.714682
-    var contentString = "asdaasdssssssssssssssssssssssssssssdasd"
 
     return(
         <React.Fragment>
 
         <GoogleMap defaultZoom={17} defaultCenter={{lat:-31.646097, lng:-60.714682}} />
         <Marker position={{lat:-31.646097, lng:-60.714682}} />
-        <InfoWindow  content={contentString} />
         </React.Fragment>
 
     )
